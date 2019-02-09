57197f5388e310b3ce6334e7efaa45d9f6569974
Alex <therebeloper@gmail.com>
2019-02-09 09:15:17 +0200

    first commit

---
+++ EasyMVVM/DispatchQueueHelper.swift
@@ -0,0 +1,33 @@
+//
+//  DispatchQueueHelper.swift
+//  EasyMVVM
+//
+//  Created by Alex Nagy on 09/02/2019.
+//  Copyright © 2019 Alex Nagy. All rights reserved.
+//
+
+import Foundation
+
+class DispatchQueueHelper {
+    
+    fileprivate init() {}
+    
+    public static func delay(bySeconds seconds: Double, dispatchLevel: DispatchLevel = .main, completion: @escaping () -> ()) {
+        let dispatchTime = DispatchTime.now() + seconds
+        dispatchLevel.dispatchQueue.asyncAfter(deadline: dispatchTime, execute: completion)
+    }
+    
+    public enum DispatchLevel {
+        case main, userInteractive, userInitiated, utility, background
+        var dispatchQueue: DispatchQueue {
+            switch self {
+            case .main:                 return DispatchQueue.main
+            case .userInteractive:      return DispatchQueue.global(qos: .userInteractive)
+            case .userInitiated:        return DispatchQueue.global(qos: .userInitiated)
+            case .utility:              return DispatchQueue.global(qos: .utility)
+            case .background:           return DispatchQueue.global(qos: .background)
+            }
+        }
+    }
+}
+
--- EasyMVVM/RootViewController.swift
+++ EasyMVVM/RootViewController.swift
@@ -6,15 +6,52 @@
 //  Copyright © 2019 Alex Nagy. All rights reserved.
 //
 
-import UIKit
+import TinyConstraints
 
 class RootViewController: UIViewController {
-
+    
+    lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = "Tap 'Fetch' to retreive the message"
+        return label
+    }()
+    
+    lazy var activityIndicator: UIActivityIndicatorView = {
+        let view = UIActivityIndicatorView(style: .gray)
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        setupNavigation()
+        setupViews()
     }
-
-
+    
+    fileprivate func setupNavigation() {
+        let resetBarButtonItem = UIBarButtonItem(title: "Reset", style: .plain, target: self, action: #selector(resetBarButtonItemTapped))
+        let fetchBarButtonItem = UIBarButtonItem(title: "Fetch", style: .done, target: self, action: #selector(fetchBarButtonItemTapped))
+        
+        navigationItem.setLeftBarButton(resetBarButtonItem, animated: false)
+        navigationItem.setRightBarButton(fetchBarButtonItem, animated: false)
+    }
+    
+    @objc fileprivate func resetBarButtonItemTapped() {
+        label.text = "Tap 'Fetch' to retreive the message"
+    }
+    
+    @objc fileprivate func fetchBarButtonItemTapped() {
+        
+    }
+    
+    fileprivate func setupViews() {
+        view.addSubview(label)
+        view.addSubview(activityIndicator)
+        label.centerInSuperview()
+        activityIndicator.topToBottom(of: label, offset: 12)
+        activityIndicator.centerXToSuperview()
+    }
+    
 }
 
