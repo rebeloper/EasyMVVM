c0c0b3cb58a465ef32d23d04888806ac171f0039
Alex <therebeloper@gmail.com>
2019-02-09 09:09:24 +0200

    Initial Commit

---
+++ EasyMVVM/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  EasyMVVM
+//
+//  Created by Alex Nagy on 09/02/2019.
+//  Copyright © 2019 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
